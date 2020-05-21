197a40bac4ca10821fe214c3205809a3f2d77c50
Detoss <devinschmidt@zoho.com>
2020-05-21 11:51:59 -0500

    price and order screen

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
 import classes from './Burger.css';
-import BurgerIngredient from './BurgerIngredient';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient Key={igKey + i} type={igKey} />;
-        });
-    });
+const burger = ( props ) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-           {transformedIngredients}
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
-            <h1></h1>
         </div>
-
     );
-
 };
 
-export default Burger;
+export default burger;

744d4377ad18da8324a66bdb25370f93f337a12c
Detoss <devinschmidt@zoho.com>
2020-07-10 18:56:18 -0500

    changed salad to Lettuce

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 
 const INGREDIENT_PRICES = {
-    salad: 0.5,
+    lettuce: 0.5,
     cheese: 0.4,
     meat: 1.3,
     bacon: 0.7,
@@ -23,7 +23,7 @@ class BurgerBuilder extends Component{
 
     state = {
         ingredients: {
-            salad: 0,
+            lettuce: 0,
             bacon: 0,
             cheese: 0,
             meat: 0,

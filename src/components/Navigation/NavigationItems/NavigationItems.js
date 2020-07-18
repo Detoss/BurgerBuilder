d09ad118785af2db743ced529527efdf1ec82c10
Detoss <devinschmidt@zoho.com>
2020-07-17 23:41:16 -0500

    Finished Nav js

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const navigationItems = () => (
+const navigationItem = () => (
 
     <ul>
         <li><a href="/"> A Link</a> </li>
     </ul>
 )
+
+export default navigationItem;

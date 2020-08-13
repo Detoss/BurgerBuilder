5cfbd4a6ec401a38bb05063f35f766133c4fbe45
Detoss <devinschmidt@zoho.com>
2020-08-13 00:41:17 -0500

    new edits

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
+class Layout extends Component {
+state = {
+    showSideDrawer: true
+}
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+
+    }
+    render () {
+        return (
     <Auxiliary>
         <Toolbar />
-        <SideDrawer />
+        <SideDrawer open={this.state.showSideDrawer} 
+        closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
-            {props.children}
+            {this.props.children}
         </main>
     </Auxiliary>
-);
+        )
+    }
+}
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,11 +3,15 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Auxiliary from '../../../hoc/Auxiliary'
 
 const sideDrawer = (props) => {
 //...
 
     return (
+<Auxiliary>
+    <Backdrop  show={props.open} clicked={props.closed}/>
         <div className={classes.SideDrawer}>
             <div className={classes.Logo}>
                 <Logo />
@@ -17,6 +21,7 @@ const sideDrawer = (props) => {
             </nav>
 
         </div>
+    </Auxiliary>
     );
 };
 

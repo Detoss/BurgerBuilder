feb400302c7a6c4f6af64d1f8ace8b1988a13cbc
Detoss <devinschmidt@zoho.com>
2020-08-19 23:14:59 -0500

    edit

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,12 +7,15 @@ import Backdrop from '../../UI/Backdrop/Backdrop'
 import Auxiliary from '../../../hoc/Auxiliary'
 
 const sideDrawer = (props) => {
-//...
+let attachedClasses = [classes.SideDrawer, classes.Close];
+if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+}
 
     return (
 <Auxiliary>
     <Backdrop  show={props.open} clicked={props.closed}/>
-        <div className={classes.SideDrawer}>
+        <div className={attachedClasses.join(' ')}>
             <div className={classes.Logo}>
                 <Logo />
             </div>
